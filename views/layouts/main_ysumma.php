9ad6bcafb5e8a8b73b228f124f748ca4cc0ec2b9
Johan Cañari <jacanari@unasolutions.com>
2017-11-16 23:15:35 -0500

    REMOVE NOT ITEM

--- views/layouts/main_ysumma.php
+++ views/layouts/main_ysumma.php
@@ -143,19 +143,8 @@ YsummaAsset::register($this);
                                 <i class="fa fa-phone"></i> Proveedores
                             </a>
                         </li>
-                        <li>
-                            <a href="tab-panel.html"><i class="fa fa-qrcode"></i> Tabs & Panels</a>
-                        </li>
-
-                        <li>
-                            <a href="table.html"><i class="fa fa-table"></i> Responsive Tables</a>
-                        </li>
-                        <li>
-                            <a href="form.html"><i class="fa fa-edit"></i> Forms </a>
-                        </li>
-
-
-                        <li>
+                        
+<!--                        <li>
                             <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
@@ -181,10 +170,10 @@ YsummaAsset::register($this);
 
                                 </li>
                             </ul>
-                        </li>
-                        <li>
+                        </li>-->
+<!--                        <li>
                             <a href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-                        </li>
+                        </li>-->
                     </ul>
                 </div>
             </nav>
